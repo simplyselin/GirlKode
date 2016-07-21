3ea8d38a2ede039fd7c87533dff7e35ea18fd07a
Ela Passarelli <epassarelli18@hb.edu>
2016-07-21 14:38:41 +0000

    changes quiz.erb, changed app control, addedresuls

--- application_controller.rb
+++ application_controller.rb
@@ -7,4 +7,19 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/quiz' do
+    erb :quiz
+  end
+
+  post '/process' do
+    @number = tally(params[:question1], params[:question2], params[:question3], params[:question4], params[:question5])
+    @decision = quiz(@number)
+    erb :results
+  end
+
+  get '/results' do
+    erb :results
+
+  end
+
 end
