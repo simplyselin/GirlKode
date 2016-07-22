ff14a2aecf408ec5c5efe81f74b669ad3017383e
Selin Edebali <sedebali23@gmail.com>
2016-07-22 13:42:48 +0000

    images

--- application_controller.rb
+++ application_controller.rb
@@ -2,14 +2,29 @@ require 'bundler'
 Bundler.require
 require_relative 'models/quizmodel.rb'
 
+require 'rubygems' # not necessary with ruby 1.9 but included for completeness
+require 'twilio-ruby'
+
+# put your own credentials here
+account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+# alternatively, you can preconfigure the client like so
+Twilio.configure do |config|
+  config.account_sid = account_sid
+  config.auth_token = auth_token
+end
+
+# and then you can create a new client without parameters
+@client = Twilio::REST::Client.new
+
 class ApplicationController < Sinatra::Base
 
   get '/' do
     erb :index
-  end
-
-  get '/new' do
-    puts "hello"
   end
 
   get '/another' do
